b8c55fd38fba44adbbf64e1254fe963661235a4f
SakifKhan98 <sakifkhan98@gmail.com>
2021-07-11 09:08:14 +0600

    added lesson update api

--- routes/course.js
+++ routes/course.js
@@ -1,4 +1,5 @@
 import express from "express";
+import formidable from "express-formidable";
 
 const router = express.Router();
 
@@ -6,13 +7,32 @@ const router = express.Router();
 import { requireSignin, isInstructor } from "../middlewares";
 
 // controllers
-import { uploadImage, removeImage, create, read } from "../controllers/course";
+import {
+  uploadImage,
+  removeImage,
+  create,
+  read,
+  uploadVideo,
+  removeVideo,
+  addLesson,
+  update,
+} from "../controllers/course";
 
 // image
 router.post("/course/upload-image", uploadImage);
 router.post("/course/remove-image", removeImage);
 // course
 router.post("/course", requireSignin, isInstructor, create);
+router.put("/course/:slug", requireSignin, update);
+
 router.get("/course/:slug", read);
+router.post(
+  "/course/video-upload/:instructorId",
+  requireSignin,
+  formidable(),
+  uploadVideo
+);
+router.post("/course/video-remove/:instructorId", requireSignin, removeVideo);
+router.post("/course/lesson/:slug/:instructorId", requireSignin, addLesson);
 
 module.exports = router;
